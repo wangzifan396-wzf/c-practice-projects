d30ce9103ff74388abce3bc38363c179b6ead2eb
wangzifan396-wzf <wangzifan396@gmail.com>
2026-07-17 20:39:21 +0800

    Add July practice projects

---
+++ work_7_17/work_7_17/work.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("I lost my cellphone!\n");
+
+    printf("Name    Age    Gender\n");
+    printf("---------------------\n");
+    printf("Jack    18     man\n");
+
+    printf("     **     \n");
+    printf("     **     \n");
+    printf("************\n");
+    printf("************\n");
+    printf("    *  *    \n");
+    printf("    *  *    \n");
+
+    return 0;
+}
