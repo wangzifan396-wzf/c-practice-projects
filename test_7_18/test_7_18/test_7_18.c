e7757afc20aaefbf85b7e3108e858214d847d97c
wangzifan396-wzf <wangzifan396@gmail.com>
2026-07-18 17:43:33 +0800

    Add July practice projects

---
+++ test_7_18/test_7_18/test_7_18.c
@@ -0,0 +1,78 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+
+//int main() 
+//{
+//	_Bool flag = true;
+//	if (flag) 
+//	{
+//		printf("Flag is true.\n");
+//	} 
+//	else 
+//	{
+//		printf("Flag is false.\n");
+//	}
+//	printf("Hello, World!\n");
+//	return 0;
+//}
+//
+
+//int main() 
+//{
+//	bool flag = true;
+//	if (flag) 
+//	{
+//		printf("Flag is true.\n");
+//	} 
+//	else 
+//	{
+//		printf("Flag is false.\n");
+//	}
+//	printf("Hello, World!\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("char: %zu\n", sizeof(char));
+//	printf("_Bool: %zu\n", sizeof(_Bool));
+//	printf("short: %zu\n", sizeof(short));
+//	printf("int: %zu\n", sizeof(int));
+//	printf("long: %zu\n", sizeof(long));
+//	printf("long long: %zu\n", sizeof(long long));
+//	printf("float: %zu\n", sizeof(float));
+//	printf("double: %zu\n", sizeof(double));
+//	printf("long double: %zu\n", sizeof(long double));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    int n;
+//    for (n = 32; n <= 127; n++)
+//    {
+//        printf("%c", n);
+//    }
+//    return 0;
+//}
+
+
+//
+//
+//int main()
+//{
+//	printf("%d\n", 'X');
+//	printf("%d\n", '#');
+//	return 0;
+//}
+
+
+
+int main()
+{
+	printf("%zu\n", strlen("c:\add\test\007\vip.c"));
+	return 0;
+}
