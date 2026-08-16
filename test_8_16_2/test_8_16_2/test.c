1bf98d1de38a83daa4b334847e1c5bb2571bd7d5
wangzifan396-wzf <wangzifan396@gmail.com>
2026-08-17 00:33:36 +0800

    8_16

--- test_8_16_2/test_8_16_2/test.c
+++ test_8_16_2/test_8_16_2/test.c
@@ -8,26 +8,55 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	int* ptr =
+//		(int*)malloc(num * sizeof(int));
+//	if (NULL != ptr)//判断ptr指针是否为空
+//	{
+//		int i = 0;
+//		for (i = 0; i < num; i++)
+//		{
+//			*(ptr + i) = 0;
+//		}
+//	}
+//	else
+//	{
+//		printf("malloc 失败\n");
+//		return 1;
+//	}
+//	free(ptr);//释放ptr所指向的动态内存
+//	ptr = NULL;
+//	return 0;
+//}
+
+
 int main()
 {
-	int num = 0;
-	scanf("%d", &num);
-	int* ptr =
-		(int*)malloc(num * sizeof(int));
-	if (NULL != ptr)//判断ptr指针是否为空
+	int* ptr = (int*)malloc(100);
+	if (ptr != NULL)
 	{
-		int i = 0;
-		for (i = 0; i < num; i++)
-		{
-			*(ptr + i) = 0;
-		}
+		//业务处理
 	}
 	else
 	{
-		printf("malloc 失败\n");
 		return 1;
 	}
-	free(ptr);//释放ptr所指向的动态内存
+	//扩展容量
+	//代码1 - 直接将realloc的返回值放到ptr中
+	ptr = (int*)realloc(ptr, 1000);//这样可以吗？(如果申请失败会如何？)
+	//代码2 - 先将realloc函数的返回值放在p中，不为NULL，再放ptr中
+	int* p = realloc(ptr, 1000);
+	if (p != NULL)
+	{
+		ptr = p;
+		p = NULL;
+	}
+	//业务处理
+	//....
+	free(ptr);
 	ptr = NULL;
 	return 0;
 }
